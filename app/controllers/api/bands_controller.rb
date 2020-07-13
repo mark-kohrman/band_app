74923a683b40a31248a20b41e62283d8bbdc8d13
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-12 21:11:23 -0500

    adding a create and update action

--- app/controllers/api/bands_controller.rb
+++ app/controllers/api/bands_controller.rb
@@ -8,4 +8,34 @@ class Api::BandsController < ApplicationController
     @band = Band.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @band = Band.create(
+    name: params[:name],
+    lead_singer: params[:lead_singer],
+    guitarist: params[:guitarist],
+    bassist: params[:bassist],
+    drummer: params[:drummer],
+    gig_rate: params[:gig_rate]
+    )
+    @band.save
+    render 'show.json.jb'
+  end
+  
+  def update
+    @band = Band.find_by(id: params[:id])
+    @band.update(
+      name: params[:name] || @band.name,
+      lead_singer: params[:lead_singer] || @band.lead_singer,
+      guitarist: params[:guitarist] || @band.guitarist,
+      bassist: params[:bassist] || @band.bassist,
+      drummer: params[:drummer] || @band.drummer,
+      gig_rate: params[:gig_rate] || @band.gig_rate
+      )
+      render 'show.json.jb'
+  end
+
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/bands" => "bands#index"
     get "bands/:id" => "bands#show"
+    post "bands" => "bands#create"
+    patch "bands/:id" => "bands#update"
   end
 end
